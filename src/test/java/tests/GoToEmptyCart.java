bd6f94df8199639c6da65794674ddbb37a679c13
aleksandrakhvadina-hub <aleksandra.khvadina@gmail.com>
2026-04-26 17:14:42 +0300

    Update GoToEmptyCart.java

--- src/test/java/tests/GoToEmptyCart.java
+++ src/test/java/tests/GoToEmptyCart.java
@@ -4,19 +4,14 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.CartPage;
-import pages.LoginPage;
 
 public class GoToEmptyCart extends BaseTest {
 
     @Test
     public void checkEmptyCart() {
-        // 1. открыть страницу и залогиниться
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
-        // 2. перейти в корзину (ничего не добавляя)
+        // 1. перейти в корзину (ничего не добавляя)
         driver.findElement(By.className("shopping_cart_link")).click();
-        // 3. проверить что корзина пуста
+        // 2. проверить что корзина пуста
         CartPage cartPage = new CartPage(driver);
         SoftAssert softAssert = new SoftAssert();
         int itemsCount = driver.findElements(By.className("cart_item")).size();
